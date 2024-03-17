307876e4e14a3d2b260426789a6e99a21a48063e
dnjswnsthd <dnjswnsthd@naver.com>
2024-03-17 19:17:03 +0900

    Add Config OpenAPIDefinition

--- src/main/java/io/cloudtype/Demo/DemoApplication.java
+++ src/main/java/io/cloudtype/Demo/DemoApplication.java
@@ -1,8 +1,12 @@
 package io.cloudtype.Demo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@OpenAPIDefinition(servers = {@Server(url = "https://port-0-mido-backend-17xco2lltcxhr4z.sel5.cloudtype.app/", description =  "Default Server URL")})
 
 @EnableJpaAuditing
 @SpringBootApplication
