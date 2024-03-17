84fbe93510e0f54745c61254962ff794a34798f4
dnjswnsthd <dnjswnsthd@naver.com>
2024-03-17 19:29:13 +0900

    Hotfix server down

--- src/main/java/io/cloudtype/Demo/DemoApplication.java
+++ src/main/java/io/cloudtype/Demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@OpenAPIDefinition(servers = {@Server(url = "https://port-0-mido-backend-17xco2lltcxhr4z.sel5.cloudtype.app/", description =  "Default Server URL")})
+@OpenAPIDefinition(servers = {@Server(url = "/", description =  "Default Server URL")})
 
 @EnableJpaAuditing
 @SpringBootApplication
